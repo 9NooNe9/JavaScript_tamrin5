66ea7a980df5b50800bdd9e1b069a0d79aee53b8
mahdi fazli <mahdifazli5459@gmail.com>
2021-10-30 02:52:17 +0330

    Added second Q's answer

--- Soal-dovom/Reshte-Ramzi.js
+++ Soal-dovom/Reshte-Ramzi.js
@@ -1,19 +1,35 @@
-// function Scripting(messageLength, repeat, message) {
-//     if (typeof messageLength !== 'number' || typeof repeat !== 'number' || typeof message !== 'string') {
-//         return `you entered a wrong input`
-//     }
-//
-//     let Alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-//         "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-//
-//     let messageLastIndex = message.length - 1;
-//
-//     for (let i = 1; i <= repeat; i++) {
-//
-//     }
-// }
+function Scripting(stringLength, repeat, string) {
+    let string1 = '';
+    let string2 = '';
 
-let str = "mahdi fazli";
+    if (stringLength < 1 || stringLength > 100 || typeof (stringLength) !== "number") {
+        return `length of the string must be between 1 and 100`
+    }
+    if (repeat < 1 || repeat > 100 || typeof (repeat) !== "number") {
+        return `numbers of iteration must be between 1 and 100`
+    }
+    if (stringLength !== string.length) {
+        return `length of the string does not equal to stringLength`
+    }
 
-str1 = str.slice(str.length - 1)
-console.log(str1)
+    for (let i = 0; i < repeat; i++) {
+        string1 = string.substr(0, stringLength - 1)
+        string = string[stringLength - 1].concat(string1)
+
+        for (let j = 0; j < stringLength; j++) {
+            let stringCharCode = string.charCodeAt(j)
+            if (stringCharCode === 122) stringCharCode = 96
+            if (stringCharCode === 90) stringCharCode = 64
+            if (stringCharCode === 57) stringCharCode = 47
+            string2 = string2.concat(String.fromCharCode(stringCharCode + 1))
+        }
+        string = string2
+        string2 = ''
+    }
+    return string
+}
+
+console.log(Scripting(5, 3, "abcde"))
+console.log(Scripting(3, 1, "abz"))
+console.log(Scripting(5, 2, "613ag"))
+console.log(Scripting(8, 6, "94zeas16"))
